704ba89d6955dea0a218d94b64edaadc8d4f4d62
Vitaly Shaposhnik <vitaly.shaposhnik@gmail.com>
2013-11-06 23:14:13 +0200

    Add fixture

--- src/Acme/FilmsBundle/DataFixtures/ORM/LoadFilmData.php
+++ src/Acme/FilmsBundle/DataFixtures/ORM/LoadFilmData.php
@@ -1,24 +1,78 @@
 <?php
 
-namespace Rice\DeckKeeperBundle\DataFixtures\ORM;
+namespace Acme\FilmsBundle\DataFixtures\ORM;
 
 use Doctrine\Common\DataFixtures\FixtureInterface;
-use Doctrine\Common\DataFixtures\OrderedFixtureInterface;
 use Doctrine\Common\Persistence\ObjectManager;
 
 use Acme\FilmsBundle\Entity\Film;
+use Acme\FilmsBundle\Entity\Category;
+use Acme\FilmsBundle\Entity\Actor;
+use Acme\FilmsBundle\Entity\Genre;
 
-class LoadFilmData implements FixtureInterface, OrderedFixtureInterface
+
+class LoadFilmData implements FixtureInterface
 {
-    public function getOrder()
-    {
-        return 10;
-    }
-
+    /**
+     * {@inheritDoc}
+     */
     public function load(ObjectManager $manager)
     {
+        // Film #1
+        $category = new Category();
+        $category->setName('Веселые');
+        $manager->persist($category);
+
+        $actor1 = new Actor();
+        $actor1->setName('Брэд Питт');
+        $manager->persist($actor1);
+
+        $actor2 = new Actor();
+        $actor2->setName('Деннис Фарина');
+        $manager->persist($actor2);
+
+        $genre = new Genre();
+        $genre->setName('Криминал');
+        $manager->persist($genre);
+
         $film = new Film();
-        $film->setName('Шрамы Мирродина');
+        $film->setName('Большой куш');
+        $film->addCategorie($category);
+        $film->addActor($actor1);
+        $film->addActor($actor2);
+        $film->addGenre($genre);
+        $manager->persist($film);
+
+
+        // Film #2
+        $category = new Category();
+        $category->setName('Для души');
+        $manager->persist($category);
+
+        $actor1 = new Actor();
+        $actor1->setName('Джейсон Флеминг');
+        $manager->persist($actor1);
+
+        $actor2 = new Actor();
+        $actor2->setName('Винни Джонс');
+        $manager->persist($actor2);
+
+        $actor3 = new Actor();
+        $actor3->setName('Вэс Блэквуд');
+        $manager->persist($actor3);
+
+        $genre2 = new Genre();
+        $genre2->setName('Триллер');
+        $manager->persist($genre2);
+
+        $film = new Film();
+        $film->setName('Карты, деньги, два ствола');
+        $film->addCategorie($category);
+        $film->addActor($actor1);
+        $film->addActor($actor2);
+        $film->addActor($actor3);
+        $film->addGenre($genre);
+        $film->addGenre($genre2);
         $manager->persist($film);
 
         $manager->flush();
