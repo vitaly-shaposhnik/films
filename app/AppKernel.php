a4a4943780d24a41c213a1fca1c67e0d2e46edd1
Vitaly Shaposhnik <vitaly.shaposhnik@gmail.com>
2013-12-01 21:50:51 +0200

    Add twitter-bootstrap

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -18,6 +18,7 @@ class AppKernel extends Kernel
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle(),
             new Acme\FilmsBundle\AcmeFilmsBundle(),
+            new Twitter\BootstrapBundle\TwitterBootstrapBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
