733c48bf13b9d0b2586b9327de5fe2c9800ea5ab
Vitaly Shaposhnik <vitaly.shaposhnik@gmail.com>
2013-12-06 23:00:53 +0200

    Add form type ActorFilmType

---
+++ src/Acme/FilmsBundle/Form/ActorFilmType.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Acme\FilmsBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+
+class ActorFilmType extends AbstractType
+{
+        /**
+     * @param FormBuilderInterface $builder
+     * @param array $options
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('name')
+            ->add('films')
+        ;
+    }
+    
+    /**
+     * @param OptionsResolverInterface $resolver
+     */
+    public function setDefaultOptions(OptionsResolverInterface $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'Acme\FilmsBundle\Entity\Actor'
+        ));
+    }
+
+    /**
+     * @return string
+     */
+    public function getName()
+    {
+        return 'actor_film';
+    }
+}
