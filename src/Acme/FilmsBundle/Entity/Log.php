ce8d36db34d5a26d9fc08978ce7ec9e0fd32cd1d
Vitaly Shaposhnik <vitaly.shaposhnik@gmail.com>
2013-12-22 23:49:07 +0200

    Add log entity

---
+++ src/Acme/FilmsBundle/Entity/Log.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace Acme\FilmsBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Log
+ *
+ * @ORM\Table("log")
+ * @ORM\Entity(repositoryClass="Acme\FilmsBundle\Entity\LogRepository")
+ */
+class Log
+{
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="description", type="text")
+     */
+    private $description;
+
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set description
+     *
+     * @param string $description
+     * @return Log
+     */
+    public function setDescription($description)
+    {
+        $this->description = $description;
+    
+        return $this;
+    }
+
+    /**
+     * Get description
+     *
+     * @return string 
+     */
+    public function getDescription()
+    {
+        return $this->description;
+    }
+}
