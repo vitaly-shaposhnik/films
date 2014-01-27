b9c8ed5bf8edf44703ae6a1a3c9e186ea400f4f8
Vitaly Shaposhnik <vitaly.shaposhnik@gmail.com>
2014-01-27 22:28:11 +0200

    Create UserBundle and customize templates

---
+++ src/Acme/UserBundle/AcmeUserBundle.php
@@ -0,0 +1,12 @@
+<?php
+namespace Acme\UserBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class AcmeUserBundle extends Bundle
+{
+    public function getParent()
+    {
+        return 'FOSUserBundle';
+    }
+}
